65133dc10610d18d0b39b91a3cc0fc6e5117e8d4
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 22:46:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,3 @@ end
 <<<<<<< HEAD
 adele_goldberg = Person.new
 alan_kay = Person.new
-=======
-cho = Person.new
-paul = Person.new
-leaon = Person.new
->>>>>>> 3c489c995a35bf81edf961b51ea3b176ec0cdadf
